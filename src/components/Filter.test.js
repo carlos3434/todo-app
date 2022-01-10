7bb36ab416ab8f8404b89205c38a15a4be7217d4
juan carlos rojas toralva <carlos3434@hotmail.com>
2022-01-10 16:54:45 -0500

    unit test click button

---
+++ src/components/Filter.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render, screen , fireEvent} from '@testing-library/react';
+import Filter from './Filter';
+
+test('renders Show All button', () => {
+  render(<Filter />);
+  const linkElement = screen.getByText(/Show All/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('should click on all button', () => {
+  render(<Filter />);
+
+  const handleShowAll = jest.fn()
+
+  fireEvent.click(getByText('Show All'))
+
+});
